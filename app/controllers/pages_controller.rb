5a47976003e080ddc4f0ab6ff717d5bf7e8031ef
Reid Laitinen <reidlaitinen@gmail.com>
2018-02-26 13:50:36 -0700

    update/edit page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,22 @@ class PagesController < ApplicationController
     @page = Page.new
   end
 
+  #GET /pages/:id/edit
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  #PUT/PATCH /pages/:id
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to page_path(@page)
+    else
+      render :edit
+    end
+  end
+
   #POST /pages
   def create
     @page = Page.new(page_params)
